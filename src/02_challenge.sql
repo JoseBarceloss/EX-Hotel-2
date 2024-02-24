cb9a71b86a939ab35324a2740fb3f518bf17b6d0
Barcelos <barcelos123@Ubuntu.myguest.virtualbox.org>
2024-02-24 20:03:03 -0300

    Requesito 1,2,3

---
+++ src/02_challenge.sql
@@ -0,0 +1,1 @@
+SELECT * FROM users WHERE last_name = 'Medhurst' OR last_name = 'Hills';
---
+++ src/03_challenge.sql
@@ -0,0 +1,3 @@
+SELECT id, first_name, last_name
+From users
+WHERE (first_name = 'Terry' or first_name = 'Demetrius') AND age = 31;
